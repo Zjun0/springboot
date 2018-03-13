b425fdf1b3469fa2790953734f59ea73b9d9683a
Zjun0 <1512387507@qq.com>
2018-03-13 15:23:33 +0800

    zj

---
+++ src/main/java/com/zj/springboot/common/utils/ReflectionUtil.java
@@ -0,0 +1,71 @@
+package com.zj.springboot.common.utils;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
+/**
+ * @author: zj
+ * @date: ${date}
+ */
+public class ReflectionUtil {
+
+    private ReflectionUtil(){
+        throw new AssertionError();
+    }
+
+    /**
+     * 通过反射取对象指定字段(属性)的值
+     * @param target 目标对象
+     * @param fileName 字段的名字
+     * @return 字段的值
+     */
+    public static Object getValue(Object target, String fileName){
+
+        Class<?> clazz = target.getClass();
+        String[] fs = fileName.split("\\.");
+
+        try{
+            for(int i = 0; i < fs.length - 1; i++) {
+                Field f = clazz.getDeclaredField(fs[i]);
+                f.setAccessible(true);
+                target = f.get(target);
+                clazz = target.getClass();
+            }
+            Field f = clazz.getDeclaredField(fs[fs.length - 1]);
+            f.setAccessible(true);
+            return f.get(target);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 通过反射给对象的指定字段赋值
+     * @param target 目标对象
+     * @param fieldName 字段的名称
+     * @param value 值
+     */
+    public static void setValue(Object target, String fieldName, Object value){
+        Class<?> clazz = target.getClass();
+        String[] fs = fieldName.split("\\.");
+        try{
+            for (int i = 0; i < fs.length -1; i++) {
+                Field f = clazz.getDeclaredField(fs[i]);
+                f.setAccessible(true);
+                Object val = f.get(target);
+                if (val == null) {
+                    Constructor<?> c = f.getType().getDeclaredConstructor();
+                    c.setAccessible(true);
+                    val = c.newInstance();
+                    f.set(target, val);
+                }
+            }
+            Field f = clazz.getDeclaredField(fs[fs.length -1]);
+            f.setAccessible(true);
+            f.set(target, value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
