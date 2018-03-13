b69855171267ca3ce358b3a85b2a73eb30bf4d9d
Zjun0 <1512387507@qq.com>
2018-03-13 16:33:25 +0800

    zj

--- src/main/java/com/zj/springboot/common/utils/MyUtil.java
+++ src/main/java/com/zj/springboot/common/utils/MyUtil.java
@@ -3,6 +3,7 @@ package com.zj.springboot.common.utils;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Comparator;
 
 /**
  * @author: zj
@@ -91,4 +92,51 @@ public class MyUtil {
             }
         }
     }
+
+    /**
+     * 使用循环实现的二分查找
+     * @param x
+     * @param key
+     * @param <T>
+     * @return
+     */
+    public static <T extends Comparable<T>> int binarySearch(T[] x, T key){
+        return binarySearch(x, 0, x.length- 1, key);
+    }
+
+    public static <T> int binarySearch(T[] x, T key, Comparator<T> comp) {
+        int low = 0;
+        int high = x.length - 1;
+        while (low <= high){
+            int mid =(low + high)>>>1;
+            int cmp = comp.compare(x[mid], key);
+            if (cmp < 0) {
+                low= mid + 1;
+            }
+            else if (cmp > 0) {
+                high= mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    private static<T extends Comparable<T>> int binarySearch(T[] x, int low, int high, T key) {
+        if(low <= high) {
+            int mid = low + ((high -low) >> 1);
+            if(key.compareTo(x[mid])== 0) {
+                return mid;
+            }
+            else if(key.compareTo(x[mid])< 0) {
+                return binarySearch(x,low, mid - 1, key);
+            }
+            else {
+                return binarySearch(x,mid + 1, high, key);
+            }
+        }
+        return -1;
+    }
+
 }
