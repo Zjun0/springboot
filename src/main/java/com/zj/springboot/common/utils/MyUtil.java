967f609ef9f14eaf66fb9b6166b31a39c90dac45
Zjun0 <1512387507@qq.com>
2018-03-13 14:36:14 +0800

    zj

--- src/main/java/com/zj/springboot/common/utils/MyUtil.java
+++ src/main/java/com/zj/springboot/common/utils/MyUtil.java
@@ -25,4 +25,29 @@ public class MyUtil {
         // 说明：调用ByteArrayInputStream或ByteArrayOutputStream对象的close方法没有任何意义
         // 这两个基于内存的流只要垃圾回收器清理对象就能够释放资源，这一点不同于对外部资源（如文件流）的释放
     }
+
+    /**
+     * 统计给定文件中给定字符串的出现次数
+     * @param filsname 文件名
+     * @param word 字符串
+     * @return 字符串在文件中出现的次数
+     */
+    public static int countWordInFile(String filsname, String word){
+        int counter = 0;
+        try (FileReader fr = new FileReader(filsname)){
+            try(BufferedReader  br = new BufferedReader(fr)){
+                String line = null;
+                while((line = br.readLine())!= null){
+                    int index = -1;
+                    while (line.length() >= word.length() && (index = line.indexOf(word)) >= 0){
+                        counter++;
+                        line = line.substring(index + word.length());
+                    }
+                }
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return counter;
+    }
 }
