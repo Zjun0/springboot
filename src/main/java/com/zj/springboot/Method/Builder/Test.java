816da7e084bd24571cbd913b0923f57be335e7bc
Zjun0 <1512387507@qq.com>
2018-03-19 23:55:25 +0800

    抽象工厂模式

---
+++ src/main/java/com/zj/springboot/Method/Builder/Test.java
@@ -0,0 +1,8 @@
+package com.zj.springboot.Method.Builder;
+
+public class Test {
+    public static void main(String[] args) {
+        Builder builder = new Builder();
+        builder.produceMailSender(10);
+    }
+}
