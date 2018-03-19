8a26cf7d783cb54130a5634821bad20a3cee64a3
Zjun0 <1512387507@qq.com>
2018-03-19 22:59:27 +0800

    工厂模式

---
+++ src/main/java/com/zj/springboot/Method/AbstractFactory/inter/Sender.java
@@ -0,0 +1,11 @@
+package com.zj.springboot.Method.AbstractFactory.inter;
+
+/**
+ * 1、普通工厂模式，就是建立一个工厂类，对实现了同一接口的一些类进行实例的创建。
+ * （我们举一个发送邮件和短信的例子）
+ */
+
+//创建二者的共同接口
+public interface Sender {
+    public void Send();
+}
