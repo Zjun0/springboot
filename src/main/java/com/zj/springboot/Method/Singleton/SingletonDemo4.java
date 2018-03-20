bbfcaafd56ebc1456a694432efc3d0dc6fb8a64a
Zjun0 <1512387507@qq.com>
2018-03-20 10:32:38 +0800

    单例模式

---
+++ src/main/java/com/zj/springboot/Method/Singleton/SingletonDemo4.java
@@ -0,0 +1,28 @@
+package com.zj.springboot.Method.Singleton;
+
+/**
+ * @author: zj
+ * @date: ${date}
+ */
+
+/**
+ * 静态类内部加载
+ */
+public class SingletonDemo4 {
+    private static class StringletonHolder{
+        private  static SingletonDemo4 instance = new SingletonDemo4();
+    }
+
+    private SingletonDemo4(){
+        System.out.println("Siingleton has loaded");
+    }
+    private static SingletonDemo4 getInstance(){
+        return StringletonHolder.instance;
+    }
+
+    /*
+    使用内部类的好处是，静态内部类不会在单例加载时就加载，
+    而是在调用getInstance()方法时才进行加载，达到了类似懒汉模式的效果，
+    而这种方法又是线程安全的。
+     */
+}
---
+++ src/main/java/com/zj/springboot/Method/Singleton/SingletonDemo5.java
@@ -0,0 +1,34 @@
+package com.zj.springboot.Method.Singleton;
+
+/**
+ * @author: zj
+ * @date: ${date}
+ */
+
+/**
+ * 枚举方法
+ */
+enum SingletonDemo5 {
+    INSTANCE;
+    public void otherMethods(){
+        System.out.println("Something");
+    }
+    /*
+    Effective Java作者Josh Bloch 提倡的方式，在我看来简直是来自神的写法。解决了以下三个问题：
+
+    (1)自由序列化。
+
+    (2)保证只有一个实例。
+
+    (3)线程安全。
+
+    如果我们想调用它的方法时，仅需要以下操作：
+
+
+    public class Hello {
+        public static void main(String[] args){
+            SingletonDemo.INSTANCE.otherMethods();
+        }
+    }
+     */
+}
