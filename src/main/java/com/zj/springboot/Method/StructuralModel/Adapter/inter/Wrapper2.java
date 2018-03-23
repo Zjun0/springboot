62a441d8b94892c8e0a1c7c3e95fe0f9a1901662
Zjun0 <1512387507@qq.com>
2018-03-23 11:05:30 +0800

    适配器模式

---
+++ src/main/java/com/zj/springboot/Method/StructuralModel/Adapter/inter/Wrapper2.java
@@ -0,0 +1,19 @@
+package com.zj.springboot.Method.StructuralModel.Adapter.inter;
+
+import com.zj.springboot.Method.StructuralModel.Adapter.inter.Targetable;
+
+/**
+ * @author: zj
+ * @date: ${date}
+ */
+public abstract class Wrapper2 implements Targetable{
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public void method2() {
+
+    }
+}
