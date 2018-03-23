c16422aca57fee9b81865ad47d308a3c3477d25f
Zjun0 <1512387507@qq.com>
2018-03-23 11:05:14 +0800

    适配器模式

---
+++ src/main/java/com/zj/springboot/Method/StructuralModel/Adapter/Impl/SourceSub1.java
@@ -0,0 +1,14 @@
+package com.zj.springboot.Method.StructuralModel.Adapter.Impl;
+
+import com.zj.springboot.Method.StructuralModel.Adapter.inter.Wrapper2;
+
+/**
+ * @author: zj
+ * @date: ${date}
+ */
+public class SourceSub1 extends Wrapper2 {
+    @Override
+    public void method1() {
+        super.method1();
+    }
+}
